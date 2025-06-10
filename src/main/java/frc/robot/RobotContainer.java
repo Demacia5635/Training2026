49300324839fcb0502b377fbbbcaeca78519149c
KislevUdi <udi.kislev@gmail.com>
2025-06-10 12:12:41 +0300

    Typo

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,6 @@ package frc.robot;
 import frc.robot.commands.MyFirstSubsystemCommand;
 import frc.robot.commands.MyFirstSubsystemTurnCommand;
 import frc.robot.subsystems.MyFirstSubsystem;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
