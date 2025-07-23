931d6de68b21722a5dfe0e23c4e9d75cbf6c7dda
YoavHaGoat <yoavga.cohen@gmail.com>
2025-07-23 19:45:50 +0300

    merge the leds into the elevatorSybsystem

--- elevatorSubsystem.java
+++ elevatorSubsystem.java
@@ -3,6 +3,7 @@ public class elevatorSubsystem {
     private double force;
     private double velocity;
     private double accleration;
+    private LedsSubsystem led;
     public elevatorSubsystem(){
         this.velocity=0;
         this.accleration=0;
@@ -26,12 +27,25 @@ public class elevatorSubsystem {
     public boolean isInMaxHeight(double currentHeight){
         return currentHeight==0.6;
     }
-    public void isInLowHeight(){
-
+    public boolean isInLowHeight(double currentHeight){
+        return currentHeight==0;
     }
     public void currentHeightAfter002sec(){
         this.currentHeight = currentHeight + velocity*0.02;
         this.velocity=velocity + 0.02*accleration;
     }
-    
+    public void setColorLedWhileMoving(){
+        if(isInLowHeight(currentHeight)){
+            led.setColor(256,256,256);
+        }
+        else if(isInMaxHeight(currentHeight)){
+            led.setColor(256,0,0);
+        }
+        else if(this.accleration==0.1){
+            led.setColor(256,0,0);
+        }
+        else{
+           led.setColor(0,256,0);
+        }
+    }
 }
