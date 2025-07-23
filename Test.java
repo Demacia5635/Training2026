86ccd21e887a7bf32bc4d8ef519bd9d0a9ad5fa9
Joel Bursztyn <Joel3141@gmail.com>
2025-07-23 17:00:28 +0300

    Adding a new feature to Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, World! From Joel");
+        System.out.println("Hello, World! From Joel second time");
     }
 
 }
