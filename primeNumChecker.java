0c709e6c5606e94c15fcc173ef9b769f8a030378
Eyal-edward-bar <eyal.bar.games@gmail.com>
2025-07-23 17:00:08 +0300

    finished primeNumChecker

--- primeNumChecker.java
+++ primeNumChecker.java
@@ -6,6 +6,7 @@ public class primeNumChecker {
         try {
             System.out.print("Enter a non-zero positive integer: ");
             int number = scanner.nextInt();
+            boolean isnotprimeassum = true;
 
             if (number == 0) {
                 System.out.println("The number must be a non-zero positive integer.");
@@ -13,12 +14,26 @@ public class primeNumChecker {
                 System.out.println("You entered 1, which is not indentifiable as prime!");
             }
             else if(number > 1) {
+                int[] divisors = new int[number];
+                int divisorCount = 0;
+
+                for (int i = 1; i <= number; i++) {
+                    if (number % i == 0) {
+                        divisors[divisorCount++] = i;
+                    }
+                }
+
+                System.out.print("The number " + number + " is divisible by: ");
+                for (int i = 0; i < divisorCount; i++) {
+                    System.out.print(divisors[i] + (i < divisorCount - 1 ? ", " : "\n"));
+                }
                 for(int i = 2; i<= number; i++) {
-                    if(number % i == 0) {
+                    if(number % i == 0 && isnotprimeassum == true) {
                         if(i == number) {
                             System.out.println("The number " + number + " is prime.");
                         } else {
                             System.out.println("The number " + number + " is not prime.");
+                            isnotprimeassum = false;
                         }
                     }
                 }
