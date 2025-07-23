022843b2d099c1cc02d16ad00c72eaca9b459e60
YoavHaGoat <yoavga.cohen@gmail.com>
2025-07-23 16:51:25 +0300

    Tirgul

---
+++ TirgulInBranches.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class TirgulInBranches {
+    public static Scanner reader=new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("YoavHaGoat");
+    }
+}
