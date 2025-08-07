d2dfa254e7f0e59a9723d7c3fed315970dfc0e71
KislevUdi <udi.kislev@gmail.com>
2025-08-07 13:27:28 +0300

    Added option for refresh all in StatusSignalData

--- src/main/java/frc/Demacia/utils/StatusSignalData.java
+++ src/main/java/frc/Demacia/utils/StatusSignalData.java
@@ -1,8 +1,15 @@
 package frc.Demacia.utils;
 
+import java.util.Arrays;
+
 import com.ctre.phoenix6.StatusSignal;
 
 public class StatusSignalData<T> {
+
+    @SuppressWarnings("rawtypes")
+    private static StatusSignal[] signals = null;
+
+
     StatusSignal<T> signal;
     double lastValue;
     double multiplier = 1;
@@ -11,6 +18,7 @@ public class StatusSignalData<T> {
         this.signal = signal;
         signal.refresh();
         lastValue = signal.getValueAsDouble();
+        add();
     }
     public StatusSignalData(StatusSignal<T> signal, double multiplier) {
         this(signal);
@@ -40,6 +48,20 @@ public class StatusSignalData<T> {
 
     public StatusSignal<T> signal() {
         return signal;
+    }
+
+    private void add() {
+        if(signals == null) {
+            signals = new StatusSignal[1];
+            signals[0] = signal;
+        } else {
+            signals = Arrays.copyOf(signals, signals.length + 1);
+            signals[signals.length-1] = signal;
+        }
+    }
+
+    public void refreshAll() {
+        StatusSignal.refreshAll(signals);
     }
 
 }
