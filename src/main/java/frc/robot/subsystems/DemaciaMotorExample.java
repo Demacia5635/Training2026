7daa5ea55a47cde0e4ae32befd5a5e2e62353582
KislevUdi <udi.kislev@gmail.com>
2025-08-09 14:54:49 +0300

    Added comments and changed to use motor.showSysidCommands

--- src/main/java/frc/robot/subsystems/DemaciaMotorExample.java
+++ src/main/java/frc/robot/subsystems/DemaciaMotorExample.java
@@ -17,21 +17,21 @@ import frc.Demacia.utils.Motors.TalonMotor;
  * Deploy to Robot
  * In Elastic:
  *  open a new TAB
- *  add the Motor Information,
- *  add the Position as Graph
+ *  set the Elastic to shoe submit buttons by default
+ *  add the Motor Information - change position/velocity to graph, set label to left
  *  check direction and degrees are correct - if needed update the configuration
  *  add the slow, random and motion command
- *  for the motion command add the target position value
+ *  for the motion command add the Motion Command: field
  *      change the position value to slider, make it longer, set the min/max values and allow updating while draggin
  *      lock the display - for draging the slider
- *  add the PID and MotionVelocities display - change the update buttom type to toggle button
+ *  add the PID and MotionVelocities display - change the update buttom type to toggle button, set label to left
  *  
  *  run the Slow power command - mark the voltage when the position starts to move - this should be KS for position (radians/degrees) motor
  *  run the Random power command - let it run for 20-30 seconds
  *  use the log data manage on the PC to transfer the log to the project logs dir
  *  run the Sysid - from Vscode select the Sysid.java file, locate the main function and select RUN
  *  select the downloaded file, select the motor and run the calculate
- *  note the gains - also check that the avg error is low, and the max error uis reasnable
+ *  note the gains - also check that the avg error is low, and the max error is reasnable
  *      records with high error are listed at the message panel
  *      also note the maximum velocity reached
  * 
@@ -45,31 +45,36 @@ import frc.Demacia.utils.Motors.TalonMotor;
  *      update the configuration of the motor
  * 
  *  re-deploy and check the motion
+ * 
+ * Note on position/radians/degrees motors:
+ *     the main issue is the accuracy - for accuracy we need ggod paramters for very slow velocities.
+ *     it is best to set the kS to the minimum power that generate movement - Static Friction vs Dynamic friction for Velocity motor
+ *     The kA can be larger - it will increase acceleration
+ *     The kP can be larger for faster correction
  *   
  */
 
 public class DemaciaMotorExample extends SubsystemBase {
+
+  public static class Example { // Should be in Constants file
+    public static final TalonConfig MOTOR_CONFIG = new TalonConfig(11, Canbus.Rio, "Talon Example")
+      .withBrake(true)
+      .withCurrent(20) // Maximum current in Apmpers
+      .withInvert(true)
+      .withDegreesMotor(12.8) // the motor gear ratio - all data in degrees
+      .withMotionParam(500, 1000, 1500) // set the motion parametrs
+      .withPID(0.01, 0, 0, 0.1, 0.0045,0.0005, 0) // the PID/FF gains
+      .withRampTime(0.2) // time from 0 to full power
+      .withVolts(7);
+}
+
     // Define the motor 
     MotorInterface motor;
 
-    
     // Constructor
     public DemaciaMotorExample() {
         super();
         motor = new TalonMotor(Example.MOTOR_CONFIG);
         motor.showSysidCommands(this);
     }
-
-    public static class Example { // Should be in Constants file
-        public static final TalonConfig MOTOR_CONFIG = new TalonConfig(11, Canbus.Rio, "Talon Example")
-          .withBrake(true)
-          .withCurrent(20) // Maximum current in ASpmpers
-          .withInvert(true)
-          .withDegreesMotor(12.8) // the motor gear ratio - all data is in degrees
-          .withMotionParam(1000, 2000, 2900) // set the motion parametrs
-          .withPID(0.01, 0, 0, 0.15, 0.003,0.001, 0) // the PID gains
-          .withRampTime(0.3) // time from 0 to full power
-          .withVolts(7);
-  }
-
 }   
