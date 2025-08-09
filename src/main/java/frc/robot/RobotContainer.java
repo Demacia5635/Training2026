1136f9925da8d5ddff27fb277a9145ae7f951a32
KislevUdi <udi.kislev@gmail.com>
2025-08-09 15:01:01 +0300

    Remove clutter from RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,52 +6,22 @@ package frc.robot;
 
 import frc.robot.subsystems.DemaciaMotorExample;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-//  private final MotorExampleSubsytem motorExample = new MotorExampleSubsytem();
-//  private final Command autoCommand = new MotorExampleCommand(motorExample);
-  private static Robot robot;
+
+  public static Robot robot;
   public static int N_CYCLE = 0;
   public static double CYCLE_TIME = 0.02;
 
-//  private final CommandXboxController controller = new CommandXboxController(0);
-//  public DriveSubsystem driveSubsystem = new DriveSubsystem(controller);
-//  public SingleModuleSubsystem singleModuleSubsystem = new SingleModuleSubsystem();
-  
   public DemaciaMotorExample demaciaMotorExample = new DemaciaMotorExample();
-  //public final ModuleSubsystem module = new ModuleSubsystem();
-  //public final MotorExampleSubsytem motorExample = new MotorExampleSubsytem();
 
-  
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer(Robot robot) {
     RobotContainer.robot = robot;
     RobotContainer.CYCLE_TIME = robot.getPeriod();
     configureBindings();
   }
-
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-
+  
    private void configureBindings() {
-//    controller.a().onTrue(aCommand);
   }
 
   public static boolean isEnabled() {
@@ -62,11 +32,6 @@ public class RobotContainer {
     N_CYCLE++;
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
     return null;
   }
