76c7d07a739e25bf9ce14d47f9ee060ef6c370ae
Yuval5635 <yuyudiamant@gmail.com>
2025-08-13 17:51:27 +0300

    Fix problem with merge

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,6 +18,37 @@ import frc.Demacia.utils.Motors.BaseMotorConfig.Canbus;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  public static class ModuleConstants {
+    public static final int STEER_ID = 1;
+    public static final int DRIVE_ID = 2;
+    public static final int CANBCODER_ID = 3;
+    public static final double MAX_STEER_AMPS = 20;
+    public static final double MAX_STEER_VOLTS = 8;
+    public static final boolean STEER_INVERTED = false;
+    public static final boolean DRIVE_INVERTED = false;
+    public static final double STEER_GERA_RATIO = 150.0/7.0;
+    public static final double DRIVE_GERA_RATIO = 6.75;
+    public static final double STEER_KP = 0.1;
+    public static final double STEER_KI = 0.0;
+    public static final double STEER_KD = 0.0;
+    public static final double STEER_KS = 0.1;
+    public static final double STEER_KV = 0.2;
+    public static final double STEER_KA = 0.01;
+    public static final double DRIVE_KP = 0.1;
+    public static final double DRIVE_KI = 0.0;
+    public static final double DRIVE_KD = 0.0;
+    public static final double DRIVE_KS = 0.1;
+    public static final double DRIVE_KV = 0.2;
+    public static final double DRIVE_KA = 0.01;
+    public static final double STEER_RAMP = 0.2;
+    public static final double DRIVE_RAMP = 0.2;
+    public static final double WHEEL_CIRCUMFERENCE = Math.PI * 4 * 0.0254;
+
+    public static final double STEER_VELOCITY_P = 2;
+    public static final double ABS_ENCODER_OFFSET = 10;
+  }
+
   public static class MotorConstants {
     public static final int DriveMotorID = 10;
     public static final int SteerMotorID = 11;
@@ -25,5 +56,25 @@ public final class Constants {
     public static final double DriveRatio = 8.14;
     public static final double WheelRadius = 0.05;
     public static final String MotorCANbus = "rio";
+  }
+
+  public static class BaseConfigs {
+    public static final TalonConfig BASE_TALON_CONFIG = new TalonConfig(0, Canbus.Rio, "base")
+        .withBrake(true)
+        .withCurrent(40)
+        .withRampTime(0.3)
+        .withVolts(12)
+        .withPID(0, 0, 0, 0, 0, 0, 0);
+
+    public static final SparkConfig BASE_SPARK_CONFIG = new SparkConfig(0, "base")
+        .withBrake(true)
+        .withCurrent(40)
+        .withRampTime(0.3)
+        .withVolts(12)
+        .withPID(0, 0, 0, 0, 0, 0, 0);
+  }
+
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
   }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -51,10 +51,11 @@ public class RobotContainer {
     // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
-  public void periodic() {
-    N_CYCLE++;
-  }
-
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     //return startCommend;
