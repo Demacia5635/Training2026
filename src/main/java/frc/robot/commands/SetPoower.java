8fabb8b86b4d537a04b3b8feccbcc4e10a32ccd1
big-y4547 <145460306+big-y4547@users.noreply.github.com>
2025-09-07 18:10:13 +0300

    2

--- src/main/java/frc/robot/commands/SetPoower.java
+++ src/main/java/frc/robot/commands/SetPoower.java
@@ -1,10 +1,14 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.SwerveModule;
 
-public class SetPoower {
+public class SetPoower extends Command {
     SwerveModule[] moudles;
+    private final double duration;
+    Timer Timer;
     public SetPoower(){
         moudles=new SwerveModule[]{
             new SwerveModule("FrontLeft"),
@@ -17,7 +21,31 @@ public class SetPoower {
         moudles[1].SetMotor(Constants.MyFirstSubsystemConstants.steer_Motor_ID_Front_right,Constants.MyFirstSubsystemConstants.Drive_Motor_ID_Front_right);
         moudles[2].SetMotor(Constants.MyFirstSubsystemConstants.steer_Motor_ID_back_left,Constants.MyFirstSubsystemConstants.Drive_Motor_ID_back_left);
         moudles[2].SetMotor(Constants.MyFirstSubsystemConstants.steer_Motor_ID_back_right,Constants.MyFirstSubsystemConstants.Drive_Motor_ID_back_right);
+        duration=10;
         
-
     }
+    @Override
+    public void initialize(){
+        double startTime = Timer.getFPGATimestamp();
+    }
+    @Override
+    public void execute(){
+        moudles[0].setPowerAll(0.1);
+        moudles[1].setPowerAll(0.1);
+        moudles[2].setPowerAll(0.1);
+        moudles[3].setPowerAll(0.1);
+    }
+    @Override
+    public boolean isFinished(){
+        return Timer.getFPGATimestamp() >= duration;
+        
+    }
+    @Override
+    public void end(boolean interrupted){
+        moudles[0].Stop();
+        moudles[1].Stop();
+        moudles[2].Stop();
+        moudles[3].Stop();
+    }
+
 }
