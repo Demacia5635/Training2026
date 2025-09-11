5006c3159654049bbd696a4def08602464e8f385
YoavHaGoat <yoavga.cohen@gmail.com>
2025-09-11 18:21:57 +0300

    noting

---
+++ src/main/java/frc/robot/subsystems/Chassis2.java
@@ -0,0 +1,98 @@
+package frc.robot;
+import static edu.wpi.first.units.Units.Radian;
+
+import com.ctre.phoenix6.hardware.Pigeon2;
+
+import edu.wpi.first.math.estimator.PoseEstimator;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.Kinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.SwerveModule;
+
+public class Chassis extends SubsystemBase{
+    SwerveModule[] moudles;
+    Pigeon2 gyro;
+    SwerveDrivePoseEstimator poseEstimator;
+    Field2d field;
+    SwerveDriveKinematics kinematicsFix;
+    public Chassis(){
+        moudles=new SwerveModule[]{
+            new SwerveModule("FrontLeft",Constants.MyFirstSubsystemConstants.steer_Motor_ID_Front_left,Constants.MyFirstSubsystemConstants.Drive_MOTOR_ID_Front_left),
+            new SwerveModule("FrontRight",Constants.MyFirstSubsystemConstants.steer_Motor_ID_Front_right,Constants.MyFirstSubsystemConstants.Drive_Motor_ID_Front_right),
+            new SwerveModule("BackLeft",Constants.MyFirstSubsystemConstants.steer_Motor_ID_back_left,Constants.MyFirstSubsystemConstants.Drive_Motor_ID_back_left),
+            new SwerveModule("BackRight",Constants.MyFirstSubsystemConstants.steer_Motor_ID_back_right,Constants.MyFirstSubsystemConstants.Drive_Motor_ID_back_right),
+
+        };
+        gyro = new Pigeon2(Constants.MyFirstSubsystemConstants.Gyro_Id,Constants.MyFirstSubsystemConstants.Gyro_Can);
+            kinematicsFix = new SwerveDriveKinematics(
+            new Translation2d[] {
+                new Translation2d(0.35, 0.3),
+                new Translation2d(0.35, -0.3),
+                new Translation2d(-0.35, 0.3),
+                new Translation2d(-0.35, -0.3)
+            }
+        );
+        poseEstimator = new SwerveDrivePoseEstimator(kinematicsFix, getAngle(), getModulePositions(), new Pose2d());
+        field = new Field2d();
+        SmartDashboard.putData("reset gyro", new InstantCommand(() -> resetGyro()).ignoringDisable(true));
+        SmartDashboard.putData("field", field);
+            
+    }
+    public void resetPose(Pose2d pose2d){
+        poseEstimator.resetPose(pose2d);
+    }
+
+
+    public void resetGyro() {
+        resetPose(new Pose2d(getPose().getTranslation(), Rotation2d.kZero));
+    }
+
+    public Rotation2d getAngle() {
+        return Rotation2d.fromRadians(gyro.getYaw().getValue().in(Radian));
+    }
+    public double getHeading() {
+        return poseEstimator.getEstimatedPosition().getRotation().getDegrees();
+    }
+
+    public Pose2d getPose(){
+        return poseEstimator.getEstimatedPosition();
+    }
+    public void setVelocities(ChassisSpeeds wantedSpeeds, ChassisSpeeds speeds){
+        ChassisSpeeds robotSpeed = ChassisSpeeds.fromFieldRelativeSpeeds(wantedSpeeds, gyro.getRotation2d());
+        SwerveModuleState[] states = kinematicsFix.toSwerveModuleStates(robotSpeed);
+        for(int i = 0; i < moudles.length; i++) {
+            moudles[i].SetState(states[i]);
+       }
+       
+
+    }
+
+    public SwerveModulePosition[] getModulePositions(){
+        SwerveModulePosition[] swerveModulesPos = new SwerveModulePosition[4];
+        for(int i = 0; i < 4; i++){
+            swerveModulesPos[i] = new SwerveModulePosition(moudles[i].GetVelocityDrive(),Rotation2d.fromDegrees(moudles[i].GetPositionOfSteer() ));
+        }
+        return swerveModulesPos;
+    }
+
+    public void periodic() {
+        poseEstimator.update(getAngle(), getModulePositions());
+        field.setRobotPose(poseEstimator.getEstimatedPosition());
+
+    }
+
+
+
+    
+
+}
---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,115 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.swerve.SwerveModuleConstants.DriveMotorArrangement;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.Demacia.utils.Motors.TalonConfig;
+import frc.Demacia.utils.Motors.TalonMotor;
+import frc.Demacia.utils.Motors.BaseMotorConfig.Canbus;
+import frc.Demacia.utils.Sensors.Cancoder;
+import frc.Demacia.utils.Sensors.CancoderConfig;
+import frc.robot.Constants;
+import static frc.robot.Constants.*;
+
+public class SwerveModule extends SubsystemBase {
+  /** Creates a new SwerveModule. */
+  private Cancoder cancoder;
+  private TalonMotor driveMotor;
+  private TalonMotor steerMotor;
+  private PIDController steerController;
+  private PIDController driveController;
+  private SimpleMotorFeedforward FF;
+
+  public SwerveModule(int cancoderId, int steerId, int driveId, String steerName, String driveName, String canCoderName) {
+    cancoder = new Cancoder(new CancoderConfig(cancoderId ,Canbus.Rio, canCoderName));
+    driveMotor = new TalonMotor(new TalonConfig(driveId , Canbus.Rio, driveName));
+    steerMotor = new TalonMotor(new TalonConfig(steerId , Canbus.Rio, steerName));
+    steerController = new PIDController(Constants.ModuleConstants.Kp, Constants.ModuleConstants.Ki, Constants.ModuleConstants.Kd);
+    driveController = new PIDController(Constants.ModuleConstants.kp2, Constants.ModuleConstants.ki2, Constants.ModuleConstants.kd2);
+    FF = new SimpleMotorFeedforward(Constants.ModuleConstants.kS, Constants.ModuleConstants.kV);
+    SmartDashboard.putData(this);
+  }
+
+
+  public void setPowerDriveMotor(double power){
+    driveMotor.set(power);
+    driveController.setTolerance(0.1);
+    driveController.setIntegratorRange(-0.1, 0.1);
+  } 
+
+
+  public void setPowerSteerMotor(double power){
+    steerMotor.set(power);
+    steerController.setTolerance(1.0);
+    steerController.setIntegratorRange(-1, 1);
+  }
+
+  public double getDriveMotorVelocity(){
+    return (driveMotor.getVelocity().getValueAsDouble()/NumberOfWheelCyclesIn1Sec)*diameterWheel*Math.PI;
+  }
+
+  public double getDriveMotorPosition(){
+    return (driveMotor.getPosition().getValueAsDouble()/NumberOfWheelCyclesIn1Sec)*diameterWheel*Math.PI;
+  }
+
+  public double getSteerMotorVelocity(){
+    return (steerMotor.getVelocity().getValueAsDouble()/Constants.ModuleConstants.Degree_ratio)*360;
+  }
+
+  public void setPositionSteer(double angle){
+    steerMotor.setPosition(angle);
+  }
+
+  public double getPositionOfSteer(){
+    return ((steerMotor.getPosition().getValueAsDouble()/Constants.ModuleConstants.Degree_ratio)*360)%360;
+  }
+
+  public void setVelocityDrive(double velocityTarget){
+    double currentVelocity = getDriveMotorVelocity();
+    double power = driveController.calculate(currentVelocity, velocityTarget) + FF.calculateWithVelocities(currentVelocity, velocityTarget);
+    setPowerDriveMotor(power);
+  }
+  public void SetSteerAngle(double targetAngle){
+    double currentAngle = getPositionOfSteer();
+    double PowerToTargetAngle = steerController.calculate(currentAngle, targetAngle);
+    setPowerSteerMotor(PowerToTargetAngle); 
+  }
+  public double getAbsouluteAngle(){
+    return cancoder.getAbsolutePosition().getValueAsDouble();
+  }
+  public void stop(){
+    driveMotor.set(0);
+    steerMotor.set(0);
+  }
+
+
+  public void SetState(SwerveModuleState state){
+        double wantedAngle = state.angle.getDegrees();
+        double velocty = state.speedMetersPerSecond;
+        setVelocityDrive(velocty);
+        SetSteerAngle(wantedAngle);
+  }
+
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    builder.addDoubleProperty("driveVelocity", this::getDriveMotorVelocity, null);
+    builder.addDoubleProperty("steerVelocity", this::getSteerMotorVelocity, null);
+    builder.addDoubleProperty("steerAngle", this::getPositionOfSteer, null);
+    builder.addDoubleProperty("DriveMotorPosition", this::getDriveMotorPosition, null);
+
+  }
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+
+  }
+}
